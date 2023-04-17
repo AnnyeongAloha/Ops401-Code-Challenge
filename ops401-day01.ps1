1d20a595ec834cf773be73d136eebf898b4616d5
Kayong <anneyoreaper@gmail.com>
2023-04-17 13:11:46 -1000

    challenge complete

---
+++ ops401-day01.ps1
@@ -0,0 +1,14 @@
+# Set the timeout value in seconds (e.g., 900 seconds = 15 minutes)
+$timeout = 900
+
+# Enable the screensaver
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveActive -Value 1
+
+# Set the screensaver timeout value
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveTimeOut -Value $timeout
+
+# Enable password protection for the screensaver
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaverIsSecure -Value 1
+
+# Apply the changes
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
