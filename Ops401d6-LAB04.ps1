f6b0f218b0da7b76b196cde9c5e7dcc2a0f5a9e4
AnnyeongAloha <116544291+AnnyeongAloha@users.noreply.github.com>
2023-04-21 00:53:10 -0700

    Update Ops401d6-LAB04.ps1

--- Ops401d6-LAB04.ps1
+++ Ops401d6-LAB04.ps1
@@ -1,4 +1,4 @@
-# SetPasswordComplexity.ps1
+# SetPasswordComplexity.ps1 (Benchmark: 1.1.5)
 
 # Define the security template file path
 $securityTemplatePath = "ComplexPassword.inf"
@@ -23,6 +23,31 @@ Remove-Item $securityTemplatePath
 
 # Output success message
 Write-Host "Password must meet complexity requirements has been set to enabled."
+
+
+# Benchmark 18.4.3
+# DisableSMBv1ClientDriver.ps1
+
+# Define the registry key path
+$regPath = "HKLM:\SYSTEM\CurrentControlSet\Services\mrxsmb10"
+
+# Define the registry value name
+$valueName = "Start"
+
+# Define the value for disabling the SMB v1 client driver
+$disableValue = 4
+
+# Check if the registry key exists
+if (Test-Path $regPath) {
+    # Set the registry value to disable the SMB v1 client driver
+    Set-ItemProperty -Path $regPath -Name $valueName -Value $disableValue
+
+    # Output success message
+    Write-Host "SMB v1 client driver has been set to Enabled: Disable driver."
+} else {
+    # Output error message
+    Write-Host "The registry key for SMB v1 client driver could not be found. Ensure the system supports SMB v1."
+}
 
 
 # source:
